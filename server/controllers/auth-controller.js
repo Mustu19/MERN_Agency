70c6a9ae138500cc38cf6d8bdd6d19f1d93e864e
Mustu19 <codewithmustu@gmail.com>
2024-03-28 04:27:47 +0530

    Storing Registered User Data in Online DB

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user-model");
+
 const home = async (req , res) => {
     try {
         res.status(200).send("Welcome to the controller home app!");    
@@ -8,8 +10,17 @@ const home = async (req , res) => {
 
 const register = async (req, res) => {
     try {
-        console.log(req.body);
-        res.status(200).json({ message: req.body})
+        const {username, email, phone, password} = req.body ;
+
+        const userExist = await User.findOne({email})
+
+        if(userExist) {
+            return res.status(400).json({msg: 'email already exists'})
+        }
+
+        const userCreated = await User.create({username, email, phone, password})
+
+        res.status(200).json({ message: userCreated})
     } catch (error) {
         res.status(500).json({ error: error})
     }
