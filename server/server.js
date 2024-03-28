86248bd3528522de51d25f4763338227772574c9
Mustu19 <codewithmustu@gmail.com>
2024-03-29 00:28:15 +0530

    Building Complete Contact Form (Schema, Route & Logics)

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const router = require('./router/auth-router');
+const authRoute = require('./router/auth-router');
+const contactRoute = require('./router/contact-router');
 const connectDb = require('./utils/db')
 const errorMiddleware = require('./middlewares/error-middleware')
 
@@ -10,7 +11,8 @@ const errorMiddleware = require('./middlewares/error-middleware')
 // Placed before any routes to ensure it is available for all subsequent route handlers
 
 app.use(express.json())
-app.use('/api/auth' , router)
+app.use('/api/auth' , authRoute)
+app.use('/api/form' , contactRoute)
 
 app.use(errorMiddleware)
 
