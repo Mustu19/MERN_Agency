430bba144f7d5eec15e1d0d40f10565092f05810
Mustu19 <codewithmustu@gmail.com>
2024-03-28 17:14:15 +0530

    Building a Complete User Login Route with Functionality

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/user-model");
+const bcrypt = require('bcryptjs')
 
 const home = async (req, res) => {
     try {
@@ -24,11 +25,39 @@ const register = async (req, res) => {
 
         const userCreated = await User.create({ username, email, phone, password })
 
-        res.status(201).json({ message: "registration successful", token: await userCreated.generateToken(), userId: userCreated._id.toString() })
+        res.status(201).json({ message: "registration successful",
+         token: await userCreated.generateToken(), 
+         userId: userCreated._id.toString() })
     }
     catch (error) {
         res.status(500).json({ error: error })
     }
 }
 
-module.exports = { home, register }
+const login = async (req, res) => {
+
+    try {
+        const { email, password } = req.body;
+
+        const userExist = await User.findOne({ email })
+        console.log(userExist)
+
+        if (!userExist) {
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+
+        const user = await bcrypt.compare(password, userExist.password)
+
+        if (user) {
+            res.status(200).json({ message: "Login successful", token: await userExist.generateToken(), userId: userExist._id.toString() })
+        }
+        else {
+            res.status(401).json({ message: "Invalid email or password" })
+        }
+    }
+    catch (error) {
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+}
+
+module.exports = { home, register, login }
