8e128e1863be9b02606abcbecf88037322bd41ca
Mustu19 <codewithmustu@gmail.com>
2024-03-28 18:37:53 +0530

    Express.js Error Handling
    Error Middleware Implemented

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const router = require('./router/auth-router');
 const connectDb = require('./utils/db')
+const errorMiddleware = require('./middlewares/error-middleware')
 
 
 // Middleware to add in order to use json in express application
@@ -10,6 +11,8 @@ const connectDb = require('./utils/db')
 
 app.use(express.json())
 app.use('/api/auth' , router)
+
+app.use(errorMiddleware)
 
 const PORT = 5000 ;
 
