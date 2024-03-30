7bed044ebaad835ba6bd956190dfbd70eb940e4d
Mustu19 <codewithmustu@gmail.com>
2024-03-30 16:54:34 +0530

    Connect React with NodeJS & MongoDB
    Stored Registration Form Data in Database

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
 
 export const Register = () => {
 
@@ -9,6 +10,8 @@ export const Register = () => {
     phone: '',
     password: ''
   })
+
+  const navigate = useNavigate()
 
   const handleInput = (e) => {
     console.log(e)
@@ -22,9 +25,35 @@ export const Register = () => {
     })
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(user);
+
+    // connecting backend with frontend
+
+    try {
+      const response = await fetch(`http://localhost:5000/api/auth/register/`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user),
+      })
+
+      if (response.ok) {
+        setUser({
+          username: '',
+          email: '',
+          phone: '',
+          password: ''
+        })
+        navigate("/login")
+      }
+
+      console.log(response);
+
+    } catch (error) {
+      console.log("register" + error)
+    }
+
   }
 
   return (
--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
 require("dotenv").config();
+const cors = require("cors")
 const express = require('express');
 const app = express();
 const authRoute = require('./router/auth-router');
 const contactRoute = require('./router/contact-router');
 const connectDb = require('./utils/db')
-const errorMiddleware = require('./middlewares/error-middleware')
+const errorMiddleware = require('./middlewares/error-middleware');
+const { coerce } = require("zod");
 
 
 // Middleware to add in order to use json in express application
 // Placed before any routes to ensure it is available for all subsequent route handlers
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json())
 app.use('/api/auth' , authRoute)
