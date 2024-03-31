12bd44c08c779e3258d4da409c6e9f401fa1462e
Mustu19 <codewithmustu@gmail.com>
2024-03-31 07:20:09 +0530

    Building Service Page Backend - Schema, Model, Route, and Controller

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const authRoute = require('./router/auth-router');
 const contactRoute = require('./router/contact-router');
+const serviceRoute = require('./router/service-router');
 const connectDb = require('./utils/db')
 const errorMiddleware = require('./middlewares/error-middleware');
 const { coerce } = require("zod");
@@ -23,6 +24,7 @@ app.use(cors(corsOptions));
 app.use(express.json())
 app.use('/api/auth' , authRoute)
 app.use('/api/form' , contactRoute)
+app.use('/api/data' , serviceRoute)
 
 app.use(errorMiddleware)
 
